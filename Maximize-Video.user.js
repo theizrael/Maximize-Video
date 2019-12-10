9f858b0e86ed8dcce9d54e0e289976fe7de4f475
theizrael <3961456+theizrael@users.noreply.github.com>
2019-12-10 00:48:36 -0700

    Update Maximize-Video.user.js

--- Maximize-Video.user.js
+++ Maximize-Video.user.js
@@ -5,7 +5,7 @@
 // @author              Theizrael
 // @homepage            https://github.com/theizrael/Maximize-Video
 // @icon                https://raw.github.com/theizrael/Stein-UserScript-JS/master/favicons.png
-// @updateURL           https://raw.githubusercontent.com/theizrael/Maximize-Video/master/Maximize-Video.user.js
+// @updateURL           https://github.com/theizrael/Maximize-Video/raw/master/Maximize-Video.user.js
 // @downloadURL         https://github.com/theizrael/Maximize-Video/raw/master/Maximize-Video.user.js
 // @supportURL          https://github.com/theizrael/Maximize-Video/issues
 // @include             *
